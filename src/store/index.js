18713e8bf2c8859266a7a66b9d37fe623808884b
chinray <1745874297@qq.com>
2022-06-08 10:39:48 +0800

    【feat】store调整

--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,11 @@
-// import { createStore } from 'vuex'
-// import getters from './getters'
-// //简单的方式
-// const modulesFiles = import.meta.globEager('./modules/*.js')
-// const modules = {}
-// // console.log(modulesFiles);
-// for (const path in modulesFiles) {
-//   const moduleName = path.replace(/(.*\/)*([^.]+).*/gi, '$2')
-//   modules[moduleName] = modulesFiles[path].default
-// }
-
-// //复杂的方式
-// // const modulesFiles = import.meta.globEager('./modules/*.js')
-// // console.log(Object.keys(modulesFiles));
-// // const modules = Object.keys(modulesFiles).reduce((modules, modulePath) => {
-// //   // const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-// //   const moduleName = modulePath.replace(/(.*\/)*([^.]+).*/gi, '$2')
-// //   const value = modulesFiles[modulePath]
-// //   modules[moduleName] = value.default
-// //   return modules
-// // }, {})
-// // console.log(modules);
-// export default createStore({
-//   modules,
-//   getters
-// })
-
 import { defineStore } from "pinia";
 import getters from './getters'
+import animation from './modules/animation'
+
 // 1.定义容器
 // 参数1： 容器的ID，必须唯一，将来pinia会把所有容器挂载到根容器
 // 参数2： 选项对象
-export default defineStore("main", {
+export const mainStore = defineStore("main", {
   /**
    * 类似于组件的data， 用来存储全局状态
    * 1. 必须是函数，这样是为了在服务端渲染的时候避免交叉请求导致的数据状态污染
@@ -39,16 +14,29 @@ export default defineStore("main", {
    */
   state: () => {
     return {
-      msg: "第一个状态数据 Pinia",
+      editMode: 'edit', // 编辑器模式 edit preview
+      canvasStyleData: { // 页面全局数据
+        width: 1200,
+        height: 740,
+        scale: 100,
+      },
+      isInEdiotr: false, // 是否在编辑器中，用于判断复制、粘贴组件时是否生效，如果在编辑器外，则无视这些操作
+      componentData: [], // 画布组件数据
+      curComponent: null,
+      curComponentIndex: null,
+      // 点击画布时是否点中组件，主要用于取消选中组件用。
+      // 如果没点中组件，并且在画布空白处弹起鼠标，则取消当前组件的选中状态
+      isClickComponent: false,
     };
   },
   /**
    * 类似于组件的computed，用来封装计算属性，有缓存的功能
    */
-  getters: getters,
+  getters,
   /**
    * 类似于组件的methods，封装业务逻辑（同步，异步都可以），修改state
    */
-  actions: {},
+  actions: {
+    ...animation.actions
+  },
 });
-
--- src/views/home/modules/Editor/index.vue
+++ src/views/home/modules/Editor/index.vue
@@ -1,1 +1,41 @@
-<template>12312</template>
+<template>
+  <div
+    id="editor"
+    class="editor"
+    :class="{ edit: props.isEdit }"
+    :style="{
+      width: changeStyleWithScale(canvasStyleData.width) + 'px',
+      height: changeStyleWithScale(canvasStyleData.height) + 'px'
+    }"
+  ></div>
+</template>
+
+<script setup>
+import { defineProps, reactive, computed } from 'vue'
+import { changeStyleWithScale } from '@/utils/translate'
+import { mainStore } from '@/store'
+const store = mainStore()
+
+const props = defineProps({
+  isEdit: {
+    type: Boolean,
+    default: true
+  }
+})
+
+const state = reactive({
+  editorX: 0,
+  editorY: 0,
+  start: {
+    // 选中区域的起点
+    x: 0,
+    y: 0
+  },
+  width: 0,
+  height: 0,
+  isShowArea: false
+})
+
+console.log(store)
+const canvasStyleData = computed(() => store.canvasStyleData)
+</script>
