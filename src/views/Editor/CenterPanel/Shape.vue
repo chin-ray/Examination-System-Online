7ca856b335ea76ceee6cb4fbe9ac9e9b373026c4
chinray <1745874297@qq.com>
2022-07-05 08:47:04 +0800

    feat: 页面调整

--- src/views/Editor/CenterPanel/Shape.vue
+++ src/views/Editor/CenterPanel/Shape.vue
@@ -42,6 +42,7 @@ const props = defineProps({
 })
 
 const isActive = computed(() => props.active && !props.element.isLock)
+
 const isDragInEditor = computed(() => store.isDragInEditor)
 const dragToComponent = computed(() => store.dragToComponent)
 
